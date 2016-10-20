7dead0b480330e84b270d3ef1773d93c65531aac
300881428 <300881428@PR-A313-07>
2016-10-19 20:07:01 -0400

    Initial Commit

--- COMP229-F2016-MidTerm-3008814281/Models/Todo.cs
+++ COMP229-F2016-MidTerm-3008814281/Models/Todo.cs
@@ -13,7 +13,7 @@ namespace COMP229_F2016_MidTerm_3008814281.Models
 
         [Required]
         [StringLength(50)]
-        public string TodoName { get; set; }
+        public string TodoDescription { get; set; }
 
         public string TodoNotes { get; set; }
 
